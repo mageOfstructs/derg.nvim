6c91ecf54a91206b60522979bfefd490b51f66ca
mageOfStructs <jason.puschnig@gmx.at>
2025-01-11 13:40:59 +0100

    lazy loading for conform

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,20 +1,26 @@
 return {
 	"stevearc/conform.nvim",
+	event = { "BufWritePre" },
+	cmd = { "ConformInfo" },
 	opts = {
-      notify_on_error = false,
-      format_on_save = function(bufnr)
-        -- Disable "format_on_save lsp_fallback" for languages that don't
-        -- have a well standardized coding style. You can add additional
-        -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
-        return {
-          timeout_ms = 500,
-          lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
-        }
-      end,
-	formatters_by_ft = {
-		lua = { "stylua" },
-		rust = { "rustfmt", lsp_format = "fallback" },
+		notify_on_error = false,
+		format_on_save = function(bufnr)
+			-- Disable "format_on_save lsp_fallback" for languages that don't
+			-- have a well standardized coding style. You can add additional
+			-- languages here or re-enable it for the disabled ones.
+			local disable_filetypes = { c = true, cpp = true }
+			return {
+				timeout_ms = 500,
+				lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+			}
+		end,
+		formatters_by_ft = {
+			lua = { "stylua" },
+			rust = { "rustfmt", lsp_format = "fallback" },
+			java = { "clang-format" },
+			c = { "clang-format" },
+			cpp = { "clang-format" },
+			markdown = { "markdownlint" },
+		},
 	},
-}
 }
