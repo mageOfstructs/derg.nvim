270c8c1ffc77b0f2788401733b1e41396590337e
mageOfStructs <jason.puschnig@gmx.at>
2025-01-30 09:18:40 +0100

    fix rest

--- lua/plugins/rest.lua
+++ lua/plugins/rest.lua
@@ -1,11 +1,4 @@
 return {
 	"rest-nvim/rest.nvim",
-	ft = "rest",
-	dependencies = {
-		"nvim-treesitter/nvim-treesitter",
-		opts = function(_, opts)
-			opts.ensure_installed = opts.ensure_installed or {}
-			table.insert(opts.ensure_installed, "http")
-		end,
-	},
+	-- ft = { "rest" },
 }
