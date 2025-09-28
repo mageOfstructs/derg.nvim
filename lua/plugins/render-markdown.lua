0be720064ff5a486d255c9fa786f036b3b5fc85a
mageOfStructs <jason.puschnig@gmx.at>
2025-09-28 22:19:16 +0200

    add fix for custom-emojis.nvim

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -8,6 +8,30 @@ return {
 	-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
 	---@module 'render-markdown'
 	---@type render.md.UserConfig
-	opts = {},
+	opts = {
+		pipe_table = {
+			cell_offset = function(ctx)
+				local offset = 0
+				local row, start_col, _, end_col = ctx.node:range()
+				local highlights = vim.api.nvim_buf_get_extmarks(
+					0,
+					-1,
+					{ row, start_col },
+					{ row, end_col },
+					{ details = true, type = "highlight" }
+				)
+				for _, conceal_highlight in ipairs(highlights) do
+					if conceal_highlight[4].hl_group == "Conceal" then
+						offset = offset - (conceal_highlight[4].end_col - conceal_highlight[3])
+						if conceal_highlight[4].virt_text then
+							offset = offset + #conceal_highlight[4].virt_text[1]
+						end
+					end
+				end
+				return offset
+			end,
+		},
+	},
 	ft = "markdown",
+	-- dir = "/home/dragon/clones/render-markdown.nvim/",
 }
