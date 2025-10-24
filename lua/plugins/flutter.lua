53657c1e7b0770e43bab922d36d4cd66fcba7013
mageOfStructs <jason.puschnig@gmx.at>
2025-10-24 21:30:41 +0200

    remove unused stuff

--- lua/plugins/flutter.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"nvim-flutter/flutter-tools.nvim",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"stevearc/dressing.nvim", -- optional for vim.ui.select
-	},
-	config = true,
-	ft = "dart",
-}
